fb08737b83b57f1f28af53cb80bdb1d6465bbb31
张经 <470778561@qq.com>
2019-01-24 18:31:05 +0800

    添加适配器模式

---
+++ structural/adapter.js
@@ -0,0 +1,26 @@
+class AC220{
+    constructor(){
+        this.outPut = 220;
+        console.log(`输出${this.outPut}v的交流电`);
+    }   
+}
+
+class powerAdapter{
+    constructor(){
+        this.adaptee = new AC220();
+    }
+    outPut(){
+        let adapterInput = this.adaptee.outPut;
+        let adapterOutput = adapterInput/44;
+        console.log(`输入${adapterInput}v的交流电，输出${adapterOutput}v的直流电`);
+    }
+}
+
+class Test {
+    constructor() {
+        this.adapterInput = new powerAdapter();
+        this.adapterInput.outPut();
+    }
+}
+
+let _Test = new Test();
