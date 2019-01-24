46305f0a16ecb03be27a5e9b754722d03586984e
张经 <470778561@qq.com>
2019-01-24 15:14:02 +0800

    添加建造者模式

---
+++ creational/builder.js
@@ -0,0 +1,50 @@
+class course{
+    constructor(){
+        this.courseName = null;
+        this.courseVideo = null;
+        this.courseArticle = null;
+    }
+}
+
+/**
+ * 课程建造者
+ */
+class courseBuilder{
+    constructor(){
+        this.course = new course();
+    }
+    buildName(name){
+        this.course.courseName = name;
+    }
+    buildVideo(name) {
+        this.course.courseVideo = name;
+    }
+    buildArticle(name) {
+        this.course.courseArticle = name;
+    }
+    build() {
+        console.log(this.course);
+        return this.course;
+    }
+}
+
+class courseCoach{
+    constructor(){
+        this.courseBuilder = new courseBuilder();
+    }
+    build(name,video,article){
+        this.courseBuilder.buildName(name);
+        this.courseBuilder.buildVideo(video);
+        this.courseBuilder.buildArticle(article);
+        return this.courseBuilder.build();
+    }
+}
+
+class Test {
+    constructor() {
+        this.courseCoach = new courseCoach();
+        this.courseCoach.build("js设计模式","js设计模式视频");
+    }
+}
+
+let _Test = new Test();
