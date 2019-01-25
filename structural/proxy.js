354510e5b59e6b79759514781cd2317e07cf7438
张经 <470778561@qq.com>
2019-01-25 15:25:39 +0800

    添加代理模式

---
+++ structural/proxy.js
@@ -0,0 +1,27 @@
+class readImage{
+    constructor(filename){
+        this.filename = filename;
+        this.loadFromDisk();//模拟初始化从硬盘加载数据
+    }
+
+    display(){
+        console.log("display..."+this.filename);
+    }
+
+    loadFromDisk(){
+        console.log("loading..."+this.filename);
+    }
+}
+
+class ProxyImage{
+    constructor(filename){
+        this.realImage = new readImage(filename);
+    }
+
+    display() {
+        this.realImage.display();
+    }
+}
+
+let _ProxyImage = new ProxyImage("1.png");
+_ProxyImage.display();
