05921ca0aea39801402d36e9e52aa65888644f66
张经 <470778561@qq.com>
2019-01-26 17:42:59 +0800

    添加注释

--- creational/factoryMethod.js
+++ creational/factoryMethod.js
@@ -19,6 +19,9 @@ class pythonCourse {
     }
 }
 
+/**
+ * （抽象工厂）：在抽象工厂类中，声明了工厂方法(Factory Method)，用于返回一个产品。抽象工厂是工厂方法模式的核心，所有创建对象的工厂类都必须实现该接口
+ */
 class courseFactory {
     constructor() {
 
@@ -28,6 +31,9 @@ class courseFactory {
     }
 }
 
+/**
+ * ConcreteFactory（具体工厂）：它是抽象工厂类的子类，实现了抽象工厂中定义的工厂方法，并可由客户端调用，返回一个具体产品类的实例
+ */
 class jsFactory extends courseFactory {
     constructor() {
         super();
@@ -37,6 +43,9 @@ class jsFactory extends courseFactory {
     }
 }
 
+/**
+ * 具体javaFactory工厂
+ */
 class javaFactory extends courseFactory {
     constructor() {
         super();
@@ -46,6 +55,9 @@ class javaFactory extends courseFactory {
     }
 }
 
+/**
+ * 具体pythonFactory工厂
+ */
 class pythonFactory extends courseFactory {
     constructor() {
         super();
