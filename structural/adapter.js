2138af3eaf09495c6f00d7b27b2fcf69e23bb7f9
张经 <470778561@qq.com>
2019-01-26 18:12:48 +0800

    添加注释

--- structural/adapter.js
+++ structural/adapter.js
@@ -1,3 +1,6 @@
+/**
+ * Adaptee（适配者类）：适配者即被适配的角色，它定义了一个已经存在的接口，这个接口需要适配，适配者类一般是一个具体类，包含了客户希望使用的业务方法，在某些情况下可能没有适配者类的源代码
+ */
 class AC220{
     constructor(){
         this.outPut = 220;
@@ -8,6 +11,9 @@ class AC220{
     }   
 }
 
+/**
+ * Adapter（适配器类）：适配器可以调用另一个接口，作为一个转换器，对Adaptee进行适配，适配器类是适配器模式的核心
+ */
 class powerAdapter{
     constructor(){
         this.adaptee = new AC220();
